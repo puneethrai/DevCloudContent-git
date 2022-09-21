612e6a9186b022ef8b170a95d243dc2a03dc3d33
Puneeth Rai <puneeth.rai@intel.com>
2022-09-21 20:30:11 +0530

    inference output neatly formatted

--- training-to-inference/run.sh
+++ training-to-inference/run.sh
@@ -7,6 +7,8 @@ OUTPUT_DIR=${5:-${OUTPUT:-"/mount_folder"}}
 UTIL_FILE=${5:-${UTIL_FILE:-"openvino_notebooks/notebooks/utils/notebook_utils.py"}}
 PY_EXTENSION="py"
 INFERENCE_FILE=$(echo "$NOTEBOOK_NAME" | sed "s/ipynb/$PY_EXTENSION/")
+PERFORMANCE_OUTPUT=${OUTPUT_DIR}/output/FP16
+INFERENCE_OUPUT=${OUTPUT_DIR}/output/inference
 
 echo "Git Repo to be cloned: ${REPO}"
 echo "Notebook to be used: ${NOTEBOOK_PATH}/${NOTEBOOK_NAME}"
@@ -37,10 +39,11 @@ pytorch_lightning
 opencv-python
 
 EOT
+
 pip install -r requirements.txt
 git clone ${REPO}
-sed -e "s/display_handle = showarray(result, display_handle)/display_handle=showarray(result, display_handle);cv2.imwrite(str(next_frame_id_to_show) + '_inference.jpeg', result)/" ${UTIL_FILE} > "${UTIL_FILE}_temp"
-sed -e "s/fps = len(image_paths) \/ duration/fps = len(image_paths)\/duration;Path('FP16').mkdir(parents=True, exist_ok=True);f = open('FP16\/performance.txt', 'w');f.write(f'Throughput: {fps:.2f} FPS\\\\nLatency: {duration:.2f} s');f.close()/" "${UTIL_FILE}_temp" > "${UTIL_FILE}_temp2"
+sed -e "s#display_handle = showarray(result, display_handle)#display_handle=showarray(result, display_handle);Path('$INFERENCE_OUPUT').mkdir(parents=True, exist_ok=True);cv2.imwrite('$INFERENCE_OUPUT' + '/' + str(next_frame_id_to_show) + '_inference.jpeg', result)#" ${UTIL_FILE} > "${UTIL_FILE}_temp"
+sed -e "s#fps = len(image_paths) / duration#fps = len(image_paths)/duration;Path('$PERFORMANCE_OUTPUT').mkdir(parents=True, exist_ok=True);f = open('$PERFORMANCE_OUTPUT/performance.txt', 'w');f.write(f'Throughput: {fps:.2f} FPS\\\\nLatency: {duration:.2f} s');f.close()#" "${UTIL_FILE}_temp" > "${UTIL_FILE}_temp2"
 rm -rf "${UTIL_FILE}_temp"
 mv "${UTIL_FILE}_temp2" ${UTIL_FILE}
 cd ${NOTEBOOK_PATH}
